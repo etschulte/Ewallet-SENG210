bff5cd18bc7fb9a4d0aaf9a08e20496ac86a28e3
Damian Gow <damiangow37@yahoo.com>
2023-06-17 13:42:19 -0400

    Testing push to remote

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -4,5 +4,5 @@ public class EWalletApp {
 	//this is the app class, has the GUI and create one object of your expense calculator class. The expense calculator class is the implementation of the Expenser interface 
 	private ArrayList<User> AllData;
 	public void CreateUser(String username, String password) {}
-
+//test
 }
